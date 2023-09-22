cc52138c70ff02f98bd0743390c5d31abde9a642
Gabriel Augusto De Vito <gabriel.dvt@hotmail.com>
2023-09-22 19:24:07 -0300

    alterações relacionadas à mentoria

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,78 +1,106 @@
-import './register.css';
-
+import { useState } from "react";
+import "./register.css";
 
 function Register() {
-    return (
-        <div className='body'>
+	// const [formState, setFormState] = useState({
+	// 	name: "",
+	// 	lastName: "",
+	// 	phone: "",
+	// 	email: "",
+	// 	userType: 1,
+	// });
+	// userType => 1 (Aluno), 2 (Tutor)
 
-            <div className='bg-image'>
-               <div className='image'>
+	const [name, setName] = useState("");
 
-               </div>
-               <div className='fade'>
+	function submitForm() {
+		if (!name.length) {
+			alert("Nome é obrigatório.");
+			return;
+		}
 
-               </div>
-            </div>
-            <div className='container-1'>
-                <div className='container-content'>
-                    <div className='logo'>
-                        <img src="/images/logoYellow.svg" alt='logo' />
-                        <p>F&F Gamimg Academy</p>
-                    </div>
+		// SE CHEGOU AQUI, VAI SUBMETER O FORMULÁRIO
+		console.log("submeteu o form");
+	}
 
-                    <h1>Entre para</h1>
-                    <h2>a melhor experiencia digital</h2>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-                    </p>
-                </div>
-            </div>
-            <div className='container-2'>
-                <div className='container-content'>
-                    <form>
-                        <h3>Seja bem-vindo</h3>
-                        <h1>Cadastrar</h1>
+	function onChangeName(evt) {
+		const name = evt.target.value;
+		setName(name);
+	}
 
-                        <label for='email'>Digite seu endereco de e-mail</label>
-                        <input type='email' id='email' name='email' /><br />
+	return (
+		<div className="body">
+			<div className="bg-image">
+				<div className="image"></div>
+				<div className="fade"></div>
+			</div>
+			<div className="container-1">
+				<div className="container-content">
+					<div className="logo">
+						<img src="/images/logoYellow.svg" alt="logo" />
+						<p>F&F Gamimg Academy</p>
+					</div>
 
-                        <div className='name'>
-                            <div className='firstName'>
-                                <label for='fname'>Nome</label>
-                                <input type='text' id='fname' name='fname' />
-                            </div>
+					<h1>Entre para</h1>
+					<h2>a melhor experiencia digital</h2>
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting
+						industry. Lorem Ipsum has been the industry's standard dummy text
+						ever since the 1500s
+					</p>
+				</div>
+			</div>
 
-                            <div className='lastName'>
-                                <label for='lname'>Sobrenome</label>
-                                <input type='text' id='lname' name='lname' />
-                            </div>
-                        </div>
+			<div className="container-2">
+				<div className="container-content">
+					<div className="form-container">
+						<h3>Seja bem-vindo</h3>
+						<h1>Cadastrar</h1>
 
-                        <label for='pnumber'>Telefone</label>
-                        <input type='tel' id='pnumber' name='pnumber' />
+						<label for="email">Digite seu endereco de e-mail</label>
+						<input type="email" id="email" name="email" required />
+						<br />
 
+						<div className="name">
+							<div className="firstName">
+								<label for="fname">Nome</label>
+								<input
+									type="text"
+									id="fname"
+									name="fname"
+									required
+									value={name}
+									onChange={onChangeName}
+								/>
+							</div>
 
-                        <div className='user-type'>
-                            <div className='container-user'>
-                                <input type='radio' id='student' name='student' />
-                                <label for='student'>Aluno</label>
-                            </div>
+							<div className="lastName">
+								<label for="lname">Sobrenome</label>
+								<input type="text" id="lname" name="lname" required />
+							</div>
+						</div>
 
+						<label for="pnumber">Telefone</label>
+						<input type="tel" id="pnumber" name="pnumber" required />
 
-                            <div className='container-user'>
-                                <input type='radio' id='tutor' name='tutor' />
-                                <label for='student'>Tutor</label>
-                            </div>
-                        </div>
+						<div className="user-type">
+							<div className="container-user">
+								<input type="radio" id="student" name="student" required />
+								<label for="student">Aluno</label>
+							</div>
 
-                        <button>Cadastrar</button>
+							<div className="container-user">
+								<input type="radio" id="tutor" name="tutor" required />
+								<label for="student">Tutor</label>
+							</div>
+						</div>
 
-                    </form>
-                </div>
-            </div>
-        </div>
-    )
+						<button onClick={submitForm}>Cadastrar</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Register;
